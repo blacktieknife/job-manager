7fd169ac4e29a662d05d44f4c7d36ea545712472
dustin <dustinerickson@gmail.com>
2018-09-10 22:22:31 -0700

    trying as widget

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import '@mdi/font/css/materialdesignicons.min.css';
 import Vue from 'vue'
 import App from './App.vue'
+import Test from './Test.vue';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.min.css';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css';
+import 'document-register-element/build/document-register-element';
+import vueCustomElement from 'vue-custom-element'
 
+Vue.use(vueCustomElement)
 Vue.use(Vuetify)
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false
+Vue.config.ignoredElements = [
+    'vue-widget'
+  ];
+
+Vue.customElement('vue-widget', App);
